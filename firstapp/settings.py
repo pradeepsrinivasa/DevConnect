58e1a08ec72faa98fc2f6fe97da4a32c851b4044
Pradeep <93724318+pradeepsrinivasa@users.noreply.github.com>
2024-09-10 15:22:09 +0530

    Update settings.py

--- firstapp/settings.py
+++ firstapp/settings.py
@@ -19,6 +19,13 @@ from django.conf import settings
 import environ
 
 
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://devconnect-pcus.onrender.com',
+]
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
