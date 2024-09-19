b0860623b28e2255df844ee8c1fb4f441d7fde2c
Pradeep <93724318+pradeepsrinivasa@users.noreply.github.com>
2024-09-19 10:37:37 +0530

    Update signals.py

--- user/signals.py
+++ user/signals.py
@@ -1,65 +1,58 @@
-from django.db.models.signals import post_save,post_delete
-from .models import User,Profile,skill
 
+from django.db.models.signals import post_save, post_delete
+from .models import User, Profile, skill
 from django.core.mail import send_mail
 from django.conf import settings
 
-def createdprofile(sender,instance,created,**kwargs):
-    print('profile signal trigered')
+
+# Signal for creating a profile after a User is created
+def createdprofile(sender, instance, created, **kwargs):
+    print('Profile signal triggered')
     if created:
-        user=instance
-        profile=Profile.objects.create(
+        user = instance
+        profile = Profile.objects.create(
             user=user,
             username=user.username,
             email=user.email,
             name=user.first_name
         )
-        
+
         subject = "Welcome to Dev Connect!"
-    message = f"Hi {user.username},\n\nWelcome to Dev Connect! We are excited to have you join our developer community. Start exploring and connecting with other developers.\n\nBest Regards,\nDev Connect Team"
+        message = f"Hi {user.username},\n\nWelcome to Dev Connect! We are excited to have you join our developer community. Start exploring and connecting with other developers.\n\nBest Regards,\nDev Connect Team"
+        
         send_mail(
             subject,
             message,
             settings.EMAIL_HOST_USER,
             [profile.email],
             fail_silently=False
-            
-            
         )
-        
-        
-        
-def updateprofile(created,sender,instance,**kwargs):
-    profile=instance
-    user=profile.user
-    
-    if created==False:
-       user.first_name=profile.name
-       user.username=profile.username
-       user.email=profile.email
-       user.save()
-        
-       print("created user")
-     
 
 
-def deleteprofile(sender,instance, **kwargs):
+# Signal for updating the User when the Profile is updated
+def updateprofile(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if not created:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+        print("User profile updated")
+
+def deleteprofile(sender, instance, **kwargs):
     try:
-     user=instance.user
-     user.delete()
-     print('deleted')
-     
+        user = instance.user
+        user.delete()
+        print('User deleted')
     except:
         pass
-    
 
-     
-    
-     
+
+# Connecting the signals
 post_save.connect(createdprofile, sender=User)
+post_save.connect(updateprofile, sender=Profile)
 post_delete.connect(deleteprofile, sender=Profile)
-post_save.connect(updateprofile,sender=Profile)
-    
-    
 
-    
